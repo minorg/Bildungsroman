1c4e9839e74237b29a5f97bd30515de1c00c830e
Minor G <minorg@server.fake>
2021-04-13 20:52:24 -0400

    use default_collection and default_institution

--- make.py
+++ make.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 
+from rdflib import URIRef
+
 from paradicms_etl._pipeline import _Pipeline
 from paradicms_etl.extractors.markdown_directory_extractor import (
     MarkdownDirectoryExtractor,
 )
 from paradicms_etl.loaders.nop_loader import NopLoader
 from paradicms_etl.loaders.rdf_file_loader import RdfFileLoader
+from paradicms_etl.models.collection import Collection
+from paradicms_etl.models.institution import Institution
+from paradicms_etl.models.rights import Rights
+from paradicms_etl.models.rights_value import RightsValue
 from paradicms_etl.transformers.markdown_directory_transformer import (
     MarkdownDirectoryTransformer,
 )
@@ -16,6 +22,34 @@ class Pipeline(_Pipeline):
 
     def __init__(self, **kwds):
         root_dir_path = Path(__file__).parent.absolute()
+
+        default_institution = Institution(
+            name="Bildungsroman",
+            rights=Rights(
+                holder=RightsValue(
+                    text="Original text Copyright Minor Gordon. Other text and images copyright their respective holders."
+                ),
+                license=RightsValue(
+                    text="CC BY-NC-SA 3.0",
+                    uri=URIRef("https://creativecommons.org/licenses/by-nc-sa/3.0/"),
+                ),
+                statement=RightsValue(
+                    text="In Copyright",
+                    uri=URIRef("http://rightsstatements.org/vocab/InC/1.0/"),
+                ),
+            ),
+            uri=MarkdownDirectoryTransformer.model_uri(
+                pipeline_id=self.ID, model_type="institution", model_id="default"
+            ),
+        )
+
+        default_collection = Collection(
+            institution_uri=default_institution.uri,
+            title="Bildungsroman",
+            uri=MarkdownDirectoryTransformer.model_uri(
+                pipeline_id=self.ID, model_type="collection", model_id="default"
+            ),
+        )
 
         _Pipeline.__init__(
             self,
@@ -29,10 +63,8 @@ class Pipeline(_Pipeline):
                 pipeline_id=self.ID,
             ),
             transformer=MarkdownDirectoryTransformer(
-                collection_title="Bildungsroman",
-                collection_uri="http://bildungsroman.minorgordon.net/collection",
-                institution_name="Bildungsroman",
-                institution_uri="http://bildungsroman.minorgordon.net/institution",
+                default_collection=default_collection,
+                default_institution=default_institution,
                 pipeline_id=self.ID,
             ),
             **kwds
